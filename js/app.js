5fa1d52d81358369820eaa7634363b063aff0ddb
funye <fun913510024@163.com>
2018-01-17 18:09:31 +0800

    new article

--- js/app.js
+++ js/app.js
@@ -108,4 +108,6 @@ window.onload = function() {
             }
         });
     };
+
+    titleOfContent();
 };
--- js/toc.js
+++ js/toc.js
@@ -1,80 +1,75 @@
 // 右侧正文导航
 
-window.onload = function() {
+// 生成toc
+function titleOfContent() {
+    console.log("toc get data start....");
+    var $postContent = document.querySelectorAll(".post-content")[0];
+    var toc = [];
+    var stack = [];
+    $postContent.childNodes.forEach(element => {
+        if (stack.length == 0) {
+            if (element.nodeName == 'H2' || element.nodeName == 'H3') {
+                stack.push(element);
+                return;
+            }
+        } else {
+            if (element.nodeName == 'H2') {
+                var $h2 = stack.shift();
+                var childs = stack;
+                toc.push({ "item": $h2, "childs": childs });
 
-    titleOfContent();
+                stack = [];
+                stack.push(element);
+                return;
+            }
+            if (element.nodeName == 'H3') {
+                stack.push(element);
+                return;
+            }
+        }
+    });
+    // console.log(toc);
+
+    // 将toc转成页面
+    var htmlStr = "<ul>";
+    toc.forEach(element => {
+        if (element.childs.length < 1) {
+            htmlStr += "<li><a href='#" + element.item.id + "'>" + element.item.textContent + "</a></li>";
+        } else {
+            htmlStr += "<li><a href='#" + element.item.id + "'>" + element.item.textContent + "</a><ul>";
+            // h3
+            element.childs.forEach(element => {
+                htmlStr += "<li><a href='#" + element.id + "'>" + element.textContent + "</a></li>";
+            });
+            htmlStr += "</ul></li>";
+        }
+    });
+    htmlStr += "</ul>";
+
+    // console.log(htmlStr);
+    document.getElementById("toc").innerHTML = htmlStr;
+    console.log("toc get data end....");
 
     tocFixed();
+}
 
-    // 生成toc
-    function titleOfContent() {
-        console.log("toc get data start....");
-        var $postContent = document.querySelectorAll(".post-content")[0];
-        var toc = [];
-        var stack = [];
-        $postContent.childNodes.forEach(element => {
-            if (stack.length == 0) {
-                if (element.nodeName == 'H2' || element.nodeName == 'H3') {
-                    stack.push(element);
-                    return;
-                }
-            } else {
-                if (element.nodeName == 'H2') {
-                    var $h2 = stack.shift();
-                    var childs = stack;
-                    toc.push({ "item": $h2, "childs": childs });
-
-                    stack = [];
-                    stack.push(element);
-                    return;
-                }
-                if (element.nodeName == 'H3') {
-                    stack.push(element);
-                    return;
-                }
-            }
-        });
-        // console.log(toc);
-
-        // 将toc转成页面
-        var htmlStr = "<ul>";
-        toc.forEach(element => {
-            if (element.childs.length < 1) {
-                htmlStr += "<li><a href='#" + element.item.id + "'>" + element.item.textContent + "</a></li>";
-            } else {
-                htmlStr += "<li><a href='#" + element.item.id + "'>" + element.item.textContent + "</a><ul>";
-                // h3
-                element.childs.forEach(element => {
-                    htmlStr += "<li><a href='#" + element.id + "'>" + element.textContent + "</a></li>";
-                });
-                htmlStr += "</ul></li>";
-            }
-        });
-        htmlStr += "</ul>";
-
-        // console.log(htmlStr);
-        document.getElementById("toc").innerHTML = htmlStr;
-        console.log("toc get data end....");
-    }
-
-    // toc位置随鼠标滚动变化
-    function tocFixed() {
-        var $toc = document.getElementsByClassName("toc-widget")[0];
-        var tocOffsetTop = $toc.offsetTop + document.getElementsByClassName("content-wrap")[0].offsetTop;
-        var tocWidth = $toc.offsetWidth;
-        // console.log(tocWidth);
-        //监听滚动事件
-        window.addEventListener('scroll', function() {
-            var hight = document.body.scrollTop || document.documentElement.scrollTop;
-            if (hight >= tocOffsetTop) {
-                // console.log("距离达到");
-                $toc.style.position = 'fixed';
-                $toc.style.top = '0';
-                $toc.style.width = tocWidth + 'px';
-            } else {
-                $toc.style.cssText = '';
-                $toc.style.position = "relative";
-            }
-        });
-    }
-};
+// toc位置随鼠标滚动变化
+function tocFixed() {
+    var $toc = document.getElementsByClassName("toc-widget")[0];
+    var tocOffsetTop = $toc.offsetTop + document.getElementsByClassName("content-wrap")[0].offsetTop;
+    var tocWidth = $toc.offsetWidth;
+    // console.log(tocWidth);
+    //监听滚动事件
+    window.addEventListener('scroll', function() {
+        var hight = document.body.scrollTop || document.documentElement.scrollTop;
+        if (hight >= tocOffsetTop) {
+            // console.log("距离达到");
+            $toc.style.position = 'fixed';
+            $toc.style.top = '0';
+            $toc.style.width = tocWidth + 'px';
+        } else {
+            $toc.style.cssText = '';
+            $toc.style.position = "relative";
+        }
+    });
+}
